903879499b0d4010d528942480edf17fe3e4e757
Yogesh-160 <nityanandrekhajha@gmail.com>
2024-04-18 21:27:52 +0530

    Update

---
+++ script.js
@@ -0,0 +1,9 @@
+ const heading =  document.createElement("h1");
+       
+       heading.innerHTML = "Hello World from JavaScript!"
+
+       const root = document.getElementById("root");
+
+       root.appendChild(heading);
+
+
