0335722dca213deeb393cd548408dcd7c3d5a265
Yogesh-160 <nityanandrekhajha@gmail.com>
2024-04-21 21:16:46 +0530

    .

--- script.js
+++
@@ -1,9 +0,0 @@
- const heading =  document.createElement("h1");
-       
-       heading.innerHTML = "Hello World from JavaScript!"
-
-       const root = document.getElementById("root");
-
-       root.appendChild(heading);
-
-
