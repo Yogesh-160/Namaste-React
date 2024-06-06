bb853b223bd48c36b24514ad2ae85e481b2274ff
Yogesh-160 <nityanandrekhajha@gmail.com>
2024-06-06 12:49:20 +0530

    Creating elements using react

---
+++ Episode 1 - Inception/app.js
@@ -0,0 +1,13 @@
+// All React Code
+const heading = React.createElement(
+    "h1", 
+    {id:"heading", style: {color: "red"}},
+     "Hello World from React!!"
+    );
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(heading);
+
+
