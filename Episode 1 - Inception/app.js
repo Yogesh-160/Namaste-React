46c8a7af8c00bc5d48c281ff50e6a336c11e47b0
Yogesh-160 <nityanandrekhajha@gmail.com>
2024-06-06 12:58:18 +0530

    Created multiple div using react:

--- Episode 1 - Inception/app.js
+++ Episode 1 - Inception/app.js
@@ -1,5 +1,6 @@
 // All React Code
-const heading = React.createElement(
+
+/*const heading = React.createElement(
     "h1", 
     {id:"heading", style: {color: "red"}},
      "Hello World from React!!"
@@ -8,6 +9,23 @@ const heading = React.createElement(
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+root.render(heading);*/
 
 
+// Creating nested div in react
+
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement(
+
+        "div", { id: "child" },
+        React.createElement
+        ("h1", 
+        {style:{color:'green',fontSize:"45px", textAlign:"center"}}, 
+        "I am H1 Tag"
+    )));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(parent);
